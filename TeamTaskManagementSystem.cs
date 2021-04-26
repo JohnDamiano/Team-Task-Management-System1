b585ea22350dcc2166c81ef177d07723b13266bd
JohnDamiano <82375854+JohnDamiano@users.noreply.github.com>
2021-04-26 10:41:55 +0800

    Update TeamTaskManagementSystem.cs

--- TeamTaskManagementSystem.cs
+++ TeamTaskManagementSystem.cs
@@ -13,7 +13,54 @@ string possition = "Team leader";
     
     Console.WriteLine("Enter Your Position");
     String Possition = Console.ReadLine();
-    
+    Console.Write("Enter Your Password:");
+
+    String Passwordinput = Console.ReadLine();
+    if (Passwordinput  ==  password)
+    {
+    	Console.WriteLine("Name:"+Member1name);
+
+        Console.WriteLine("Project:"+Project);
+
+        Console.WriteLine("Possition:"+possition);
+    	Console.WriteLine("Assignment:"+Assignment);
+    		
+    	}
+    else
+    {
+    	
+       Console.WriteLine("Wrong password try again! ");
     }
+   {
+   	 string assignment1 = "-Coder";
+   	 string assignment2 = "-Editor";
+   	 string assignment3 = "-Finance";
+   	 
+     string name1 = "member1";	
+     string name2 =	"member2";
+     string name3 ="member3";
+     
+     
+     Console.WriteLine("");
+     Console.WriteLine("");
+     Console.WriteLine("Add member:");
+     
+     string member1 = Console.ReadLine();
+     string member2 = Console.ReadLine();
+     string member3 = Console.ReadLine();
+     
+     Console.WriteLine("");
+     Console.WriteLine("member1: "+member1+assignment1);
+     Console.WriteLine("member2: "+member2+assignment2);
+     Console.WriteLine("member3: "+member3+assignment3);
+    }
+     
+   
+     
+     
+     
+     
+     
+        }     }
 
 }        
